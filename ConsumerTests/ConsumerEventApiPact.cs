f8ebb4f59464e4b54f7476687f4f296566f9bafe
Giampaolo Paderno <giampaolo.paderno@citrus.nl>
2018-11-29 12:42:42 +0100

    first commit

---
+++ ConsumerTests/ConsumerEventApiPact.cs
@@ -0,0 +1,57 @@
+﻿using PactNet;
+using PactNet.Mocks.MockHttpService;
+using PactNet.Models;
+using System;
+using System.IO;
+
+namespace ConsumerTests
+{
+    public class ConsumerEventApiPact : IDisposable
+    {
+        public IPactBuilder PactBuilder { get; }
+        public IMockProviderService MockProviderService { get; }
+        public int MockServerPort => 9222;
+        public string MockProviderServiceBaseUri => $"http://localhost:{MockServerPort}";
+
+        public ConsumerEventApiPact()
+        {
+            PactBuilder = new PactBuilder(new PactConfig
+            {
+                SpecificationVersion = "2.0.0",
+                LogDir = $"..{Path.DirectorySeparatorChar}..{Path.DirectorySeparatorChar}..{Path.DirectorySeparatorChar}logs{Path.DirectorySeparatorChar}",
+                PactDir = $"..{Path.DirectorySeparatorChar}..{Path.DirectorySeparatorChar}..{Path.DirectorySeparatorChar}pacts{Path.DirectorySeparatorChar}"
+            })
+            .ServiceConsumer("Consumer")
+            .HasPactWith("Provider");
+
+            MockProviderService = PactBuilder.MockService(MockServerPort, false, IPAddress.Any);
+        }
+
+        #region IDisposable Support
+
+        // To detect redundant calls
+        private bool disposedValue = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // This will save the pact file once finished.
+                    PactBuilder.Build();
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+        }
+        #endregion
+    }
+}
